91852e65b6f1ba6dc3343d83c8dc37cdda5c2e90
Arun-ez <arunshaw433@gmail.com>
2023-08-31 11:50:06 +0530

    added login page

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,8 +1,20 @@
-import React from 'react';
+import classes from "@/styles/login.module.css";
 import Head from 'next/head';
 import Body from '@/components/Body';
 
+
 const login = () => {
+
+    const brands = [
+        '/assets/kotak.png',
+        '/assets/angel.png',
+        '/assets/uptax.png',
+        '/assets/amei.png',
+        '/assets/landbox.png',
+        '/assets/liquiloans.png',
+    ]
+
+
     return (
         <>
             <Head>
@@ -10,7 +22,27 @@ const login = () => {
             </Head>
 
             <Body>
+                <section className={classes.section4}>
+                    <div>
+                        <p> Some partners who make <br /> InvestPe products possible— </p>
+                        <div className={classes.brands}>
+                            {brands.map((src, index) => {
+                                return (
+                                    <img src={src} alt="logo" key={index} />
+                                )
+                            })}
+                        </div>
+                    </div>
 
+                    <div>
+                        <p> ...and a few of the <br /> businesses building some <br /> incredible solutions with <br /> InvestPe </p>
+
+                        <div>
+                            <img src="/assets/siply.png" alt="siply" />
+                            <img src="/assets/jupiter.png" alt="jupiter" />
+                        </div>
+                    </div>
+                </section>
             </Body>
         </>
     )
